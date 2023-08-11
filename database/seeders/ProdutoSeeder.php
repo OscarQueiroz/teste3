b8d58ff68feaf784b03d00a85dfb85039268fc5c
OscarQueiroz <jrqueiroz225@gmail.com>
2023-08-11 19:29:09 -0300

    Initial commit

---
+++ database/seeders/ProdutoSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProdutoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('produtos')->insert([
+            'nome'=> 'detergente',
+            'quantidade'=> 50,
+            'preco'=> 10.50,
+        ]);
+    }
+}
